344568947250f5a5a1f6ac2a5d2dae0877e5f541
Пялиска <81359036+cfifgzkbcjd15@users.noreply.github.com>
2023-12-18 08:40:32 +0300

    Update LimeHdTvContext.cs

--- TVPrograms/Data/LimeHdTvContext.cs
+++ TVPrograms/Data/LimeHdTvContext.cs
@@ -26,7 +26,7 @@ namespace TVPrograms.Data
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=LimeHdTv;Username=postgres;Password=ihesop69");
+                optionsBuilder.UseNpgsql("");
             }
         }
 
